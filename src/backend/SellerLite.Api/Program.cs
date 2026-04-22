334f6d0892803e651bdc7aaea87cd781f5116405
dattd26 <ducdat05112004@gmail.com>
2026-04-22 16:04:50 +0700

    chore: configure CORS

--- src/backend/SellerLite.Api/Program.cs
+++ src/backend/SellerLite.Api/Program.cs
@@ -12,6 +12,17 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -20,7 +31,9 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 
